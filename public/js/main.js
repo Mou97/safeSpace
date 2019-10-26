ee5db857b9361ae6a15b7d9dea7c5cfb0853bc77
Mou97 <m.taguiouine@esi-sba.dz>
2019-10-26 09:54:04 +0100

    baba

--- public/js/main.js
+++ public/js/main.js
@@ -2,23 +2,23 @@
 $(document).ready(function () {
 
     console.log('script loaded')
-    let speach = 'i hate you dude'
-    let url = "http://127.0.0.1:5000/api?data=" + speach
+    let speech = 'i hate you dude'
+    let url = "http://127.0.0.1:5000/api?data=" + speech
     fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log(data)
             console.log(data == "Hate speech detected.")
             if (data == "Hate speech detected.") {
-                JSON.stringify({ report: data })
+                // JSON.stringify({ report: data + ' : ' + speech })
 
-                $.post("/saveReport", { 'report': data }, function (result) {
+                $.post("/saveReport", { 'report': data + ' : ' + speech }, function (result) {
                     console.log(result)
 
                     $('.report_tb tbody').append(`
                     
                     <tr>
-                      <td colspan="2" class="text">${data}</td>
+                      <td colspan="2" class="text">${data + ' : ' + speech}</td>
                       <td>11:50 AM</td>
                       <td><i class="fa fa-ellipsis-v"></i></td>
                     </tr>
