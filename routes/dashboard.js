b3187d266271c62b8a332cc0c8fa735ecf88e1b0
ilovax <tasfaoutabderrahimilovax@gmail.com>
2019-12-14 18:52:55 +0200

    added front of stats in dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const provenDb = require('@southbanksoftware/provendb-node-driver');
+const Report = require('../controllers/reportHelper');
 //require controllers
 
 //require middleware
 
 //handling requests
-router.get("/", (req, res) => {
+router.get("/", async(req, res) => {
+    let reports = await Report.getAllReports()
+    console.log(reports);
     res.render('dashboard')
 });
 module.exports = router;
